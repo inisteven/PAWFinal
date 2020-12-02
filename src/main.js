80d90e863974c4ea44dd5000b99f46075adabca6
lawrenxiusbenny <lawrenxiusbenny@gmail.com>
2020-12-02 22:19:38 +0700

    admin

--- src/main.js
+++ src/main.js
@@ -6,8 +6,8 @@ import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import axios from 'axios';
 
- 
 
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
@@ -17,6 +17,10 @@ Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 
 Vue.config.productionTip = false
+
+Vue.prototype.$http = axios;
+Vue.prototype.$api = 'http://127.0.0.1:8000/api';
+Vue.prototype.$url = 'http://127.0.0.1:8000/';
 
 new Vue({
   vuetify,
