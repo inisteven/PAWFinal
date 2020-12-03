fe9b64c6b6b2fee18d5a39017496f2a7234187ea
orznmi <ornaoza@gmail.com>
2020-12-03 16:01:19 +0700

    halamanLogin

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,55 +1,66 @@
 <template>
   <div id="app">
     <navbar-component></navbar-component>
-    <v-container class="login">
-      <v-layout row class="text-xs-center justify-center">
-        <v-container style="position: center; top: 13%">
-          <v-card xs4 class="mx-auto" max-width="400" style="padding: 16px; margin: 40px; opacity: 0.9">
-            <div class="text-center">
-              <h2>Sign In</h2>
-            </div>
-            <br />
-            <v-form>
-              <subtitle-1>Email</subtitle-1>
-              <v-text-field solo name="Email" label="Email"></v-text-field>
-              <subtitle-1>Password</subtitle-1>
-              <v-text-field solo name="Password" label="Password" type="password"></v-text-field>
-              <v-card-actions>
-                <v-btn primary>Cancel</v-btn>
-                <v-spacer></v-spacer>
-                <v-btn class="blue lighten-3">Sign in</v-btn>
-              </v-card-actions>
-              <br />
-              <div class="text-center">
-                <subtitle-2 class="text-center">Don't have account? <router-link to="signUp">Sign Up Here</router-link></subtitle-2>
-              </div>
-            </v-form>
-          </v-card>
-        </v-container>
-      </v-layout>
-    </v-container>
+    <div class="back">
+      <v-container class="signup">
+        <v-row align="center" justify="center">
+          <v-col class="col-md-5 col-xs-0">
+            <v-card style="padding: 16px; margin: 40px; opacity: 0.9">
+              <v-card-title>
+                <h2 class="text-center">Sign In</h2>
+              </v-card-title>
+              <v-card-body>
+                <v-form>
+                  <subtitle-1>Email</subtitle-1>
+                  <v-text-field solo name="Email" label="Email"></v-text-field>
+                  <subtitle-1>Password</subtitle-1>
+                  <v-text-field
+                    solo
+                    name="Password"
+                    label="Password"
+                    type="password"
+                  ></v-text-field>
+                  <v-card-actions>
+                    <v-btn primary>Cancel</v-btn>
+                    <v-spacer></v-spacer>
+                    <v-btn class="blue lighten-3">Sign in</v-btn>
+                  </v-card-actions>
+                  <br />
+                  <div class="text-center">
+                    <subtitle-2 class="text-center"
+                      >Don't have account?
+                      <router-link to="signUp"
+                        >Sign Up Here</router-link
+                      ></subtitle-2
+                    >
+                  </div>
+                </v-form>
+              </v-card-body>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </div>
     <footer-component></footer-component>
   </div>
 </template>
 
+<script>
+import Header from "./Navbar.vue";
+import Footer from "./Footer.vue";
+export default {
+  components: {
+    "navbar-component": Header,
+    "footer-component": Footer,
+  },
+};
+</script>
 <style>
-.login {
+.back {
   background-image: url(../assets/signIn.png);
-  width: 5000px;
   background-attachment: fixed;
   background-size: cover;
   position: center;
   background-repeat: no-repeat;
 }
 </style>
-
-<script>
-import Header from './Navbar.vue';
-import Footer from './Footer.vue';
-export default {
-    components: {
-      'navbar-component' : Header,
-      'footer-component' : Footer,
-    }
-}
-</script>
--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -2,80 +2,106 @@
   <div id="app">
     <navbar-component></navbar-component>
     <div class="back">
-    <v-container class="signup">
-          <v-row align="center" justify="center">
-            <v-col class="col-md-5 col-xs-0">
-              <v-card  style="padding: 30px; opacity: 0.9">
-                <v-card-title>
-                  <div class="text-center mb-5">
-                    <h2>Create Account</h2>
-                  </div>
-                </v-card-title>
-                <v-card-body>
-                  <v-form>
-                    <v-row>
-                      <v-col class="col-xs-2">
-                        <subtitle-1>First Name*</subtitle-1>
-                        <v-text-field solo name="FirstName" label="First Name" required></v-text-field>
-                      </v-col>
-                    </v-row>
-                    <v-row>
-                      <v-col class="col-xs-2">
-                        <subtitle-1>Last Name*</subtitle-1>
-                        <v-text-field solo name="LastName" label="Last Name" required></v-text-field>
-                      </v-col>
-                    </v-row>
-                    <v-row>
-                      <v-col class="col-xs-2">
-                        <subtitle-1>Email</subtitle-1>
-                        <v-text-field solo name="Email" label="Email" required></v-text-field>
-                      </v-col>
-                    </v-row>
-                    <v-row>
-                      <v-col class="col-xs-2">
-                        <subtitle-1>Password</subtitle-1>
-                        <v-text-field solo name="Password" type="password" label="Password" required></v-text-field>
-                      </v-col>
-                    </v-row>
-                    <v-row>
-                      <v-col class="col-xs-2">
-                        <subtitle-1>Confirm Password</subtitle-1>
-                        <v-text-field solo name="confPass" type="password" label="Confirm Password" required></v-text-field>
-                      </v-col>
-                    </v-row>
-                    <v-card-actions>
-                      <v-btn primary>Cancel</v-btn>
-                      <v-spacer></v-spacer>
-                      <v-btn class="blue lighten-3">Sign Up</v-btn>
-                    </v-card-actions>
-                  </v-form>
-                </v-card-body>
-              </v-card>
-            </v-col>
-          </v-row>
-    </v-container>
+      <v-container class="signup">
+        <v-row align="center" justify="center">
+          <v-col class="col-md-5 col-xs-0">
+            <v-card style="padding: 30px; opacity: 0.9">
+              <v-card-title>
+                <div class="text-center mb-5">
+                  <h2 class="text-center">Create Account</h2>
+                </div>
+              </v-card-title>
+              <v-card-body>
+                <v-form>
+                  <v-row>
+                    <v-col class="col-xs-2">
+                      <subtitle-1>First Name*</subtitle-1>
+                      <v-text-field
+                        solo
+                        name="FirstName"
+                        label="First Name"
+                        required
+                      ></v-text-field>
+                    </v-col>
+                  </v-row>
+                  <v-row>
+                    <v-col class="col-xs-2">
+                      <subtitle-1>Last Name*</subtitle-1>
+                      <v-text-field
+                        solo
+                        name="LastName"
+                        label="Last Name"
+                        required
+                      ></v-text-field>
+                    </v-col>
+                  </v-row>
+                  <v-row>
+                    <v-col class="col-xs-2">
+                      <subtitle-1>Email</subtitle-1>
+                      <v-text-field
+                        solo
+                        name="Email"
+                        label="Email"
+                        required
+                      ></v-text-field>
+                    </v-col>
+                  </v-row>
+                  <v-row>
+                    <v-col class="col-xs-2">
+                      <subtitle-1>Password</subtitle-1>
+                      <v-text-field
+                        solo
+                        name="Password"
+                        type="password"
+                        label="Password"
+                        required
+                      ></v-text-field>
+                    </v-col>
+                  </v-row>
+                  <v-row>
+                    <v-col class="col-xs-2">
+                      <subtitle-1>Confirm Password</subtitle-1>
+                      <v-text-field
+                        solo
+                        name="confPass"
+                        type="password"
+                        label="Confirm Password"
+                        required
+                      ></v-text-field>
+                    </v-col>
+                  </v-row>
+                  <v-card-actions>
+                    <v-btn primary>Cancel</v-btn>
+                    <v-spacer></v-spacer>
+                    <v-btn class="blue lighten-3">Sign Up</v-btn>
+                  </v-card-actions>
+                </v-form>
+              </v-card-body>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
     </div>
-    <footer-component></footer-component> 
+    <footer-component></footer-component>
   </div>
 </template>
 
 <script>
-  import Header from './Navbar.vue';
-  import Footer from './Footer.vue';
-  export default {
-      components: {
-        'navbar-component' : Header,
-        'footer-component' : Footer,
-      }
-  }
+import Header from "./Navbar.vue";
+import Footer from "./Footer.vue";
+export default {
+  components: {
+    "navbar-component": Header,
+    "footer-component": Footer,
+  },
+};
 </script>
 <style>
-
-.back{
+.back {
   background-image: url(../assets/signUp.jpg);
-      background-attachment: fixed;
-    background-size: cover;
-    position: center;
-    background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+  position: center;
+  background-repeat: no-repeat;
 }
 </style>
